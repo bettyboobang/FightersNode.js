cfb7810a33a084c241c0b94ec081e8cea396d08e
bettyboobang <zorasolaryefi@gmail.com>
2026-06-01 22:59:03 +0300

    Updated fighterService and userService

--- services/fighterService.js
+++ services/fighterService.js
@@ -1,9 +1,55 @@
 import { fighterRepository } from "../repositories/fighterRepository.js";
 
 class FighterService {
-  // TODO: Implement methods to work with fighters
+  getAll() {//get all fighters
+    const fighters = fighterRepository.getAll();
+    if (!fighters) {
+      return [];
+    }
+    return fighters;
+  }
+  getOne(id) {//get fighter by id
+    const fighter = fighterRepository.getOne({ id });
+    return fighter || null;
+  }
+  create(fighterData) {//create new fighter
+    const eixstingFighter = fighterRepository.getOne({//check if there is already a fighter with the same name
+      name: (name) => name.toLowerCase() === fighterData.name.toLowerCase()
+    });
+
+    if (eixstingFighter) {//if there is a fighter with the same name we throw an error
+      throw new Error(`Fighter with name "${fighterData.name}" already exists`);
+    }
+    if (fighterData.health === undefined || fighterData.health === null) {//if health is not provided we set it to 85 by default
+      fighterData.health = 85;
+    }
+    const newFighter = fighterRepository.create(fighterData);//create new fighter with provided data and return it
+    return newFighter;
+  }
+  update(id, dataToUpdate) {//update fighter by id with provided data
+    const fighter = this.getOne(id);//check if there is a fighter with such id
+    if (!fighter) {
+      return null;
+    }
+    if (dataToUpdate.name) {//check if there is a fighter with the same name as the new name provided in dataToUpdate
+      const duplicateFighter = fighterRepository.getOne({
+        name: (name) => name.toLowerCase() === dataToUpdate.name.toLowerCase()
+      });
+      if (duplicateFighter && duplicateFighter.id !== id) {//if there is a fighter with the same name and it is not the same fighter we are trying to update
+        throw new Error(`Fighter with name "${dataToUpdate.name}" already exists`);
+      }
+    }
+    const updatedFighter = fighterRepository.update(id, dataToUpdate);
+    return updatedFighter;
+  }
+  delete(id) {//delete fighter by id
+    const fighter = this.getOne(id);
+    if (!fighter) {
+      return null;
+    }
+    const deletedFighter = fighterRepository.delete(id);//delete fighter by id and return the deleted data
+    return deletedFighter;
+  }
 }
-
 const fighterService = new FighterService();
-
 export { fighterService };
--- services/userService.js
+++ services/userService.js
@@ -1,9 +1,58 @@
 import { userRepository } from "../repositories/userRepository.js";
 
 class UserService {
-  // TODO: Implement methods to work with user
-
-  search(search) {
+  getAll() {//get all users
+    const users = userRepository.getAll();
+    if (!users) {
+      return [];
+    }
+    return users;
+  }
+  getOne(id) {//get user by id
+    const user = userRepository.getOne({ id });
+    return user || null;
+  }
+  create(userData) {//create new user
+    const emailExists = userRepository.getOne({ email: userData.email });//check if there is already a user with the same email
+    if (emailExists) {
+      throw new Error(`User with email "${userData.email}" already exists`);
+    }
+    const phoneExists = userRepository.getOne({ phone: userData.phone });//check if there is already a user with the same phone number
+    if (phoneExists) {
+      throw new Error(`User with phone number "${userData.phone}" already exists`);
+    }
+    const newUser = userRepository.create(userData);//create new user with provided data and return it
+    return newUser;
+  }
+  update(id, dataToUpdate) {//update user by id with provided data
+    const user = this.getOne(id);
+    if (!user) {
+      return null;
+    }
+    if (dataToUpdate.email) {//check if there is a user with the same email as the new email provided in dataToUpdate
+      const duplicateEmail = userRepository.getOne({ email: dataToUpdate.email });
+      if (duplicateEmail && duplicateEmail.id !== id) {//if there is a user with the same email and it is not the same user we are trying to update
+        throw new Error(`User with email "${dataToUpdate.email}" already exists`);
+      }
+    }
+    if (dataToUpdate.phone) {//check if there is a user with the same phone number as the new phone number provided in dataToUpdate
+      const duplicatePhone = userRepository.getOne({ phone: dataToUpdate.phone });
+      if (duplicatePhone && duplicatePhone.id !== id) {
+        throw new Error(`User with phone number "${dataToUpdate.phone}" already exists`);
+      }
+    }
+    const updatedUser = userRepository.update(id, dataToUpdate);//update user by id with provided data and return the updated data
+    return updatedUser;
+  }
+  delete(id) {//delete user by id
+    const user = this.getOne(id);
+    if (!user) {
+      return null;
+    }
+    const deletedUser = userRepository.delete(id);
+    return deletedUser;
+  }
+  search(search) {//search for users by email or phone number
     const item = userRepository.getOne(search);
     if (!item) {
       return null;
@@ -11,7 +60,5 @@ class UserService {
     return item;
   }
 }
-
 const userService = new UserService();
-
 export { userService };
