1799b0277ef0756a079a7acc22966aef62717209
bettyboobang <zorasolaryefi@gmail.com>
2026-06-01 23:08:12 +0300

    Done optional tasks and updated fightRoutes and fightService

--- routes/fightRoutes.js
+++ routes/fightRoutes.js
@@ -1,13 +1,51 @@
 import { Router } from "express";
 import { fightersService } from "../services/fightService.js";
-import {
-  createUserValid,
-  updateUserValid,
-} from "../middlewares/user.validation.middleware.js";
 import { responseMiddleware } from "../middlewares/response.middleware.js";
 
 const router = Router();
-
-// OPTIONAL TODO: Implement route controller for fights
-
+router.get(//get all fights
+  "/",
+  (req, res, next) => {
+    try {//call the getAll method from fightersService and store the result in res.data for our responseMiddleware
+      const data = fightersService.getAll();
+      res.data = data;
+    } catch (err) {
+      res.err = err;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
+router.get(//get fight by id
+  "/:id",
+  (req, res, next) => {
+    try {//
+      const data = fightersService.getOne(req.params.id);
+      if (!data) {
+        throw new Error("Fight not found");
+      }
+      res.data = data;
+    } catch (err) {
+      res.err = err;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
+router.post(//create new fight
+  "/",
+  (req, res, next) => {
+    try {//call the create method from fightersService with request body and store the result in res.data for our responseMiddleware
+      const data = fightersService.create(req.body);
+      res.data = data;
+    } catch (err) {
+      res.err = err;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
 export { router };
--- services/fightService.js
+++ services/fightService.js
@@ -1,9 +1,29 @@
 import { fightRepository } from "../repositories/fightRepository.js";
 
 class FightersService {
-  // OPTIONAL TODO: Implement methods to work with fights
+  getAll() {//get all fights
+    const fights = fightRepository.getAll();
+    if (!fights) {
+      return [];
+    }
+    return fights;
+  }
+  getOne(id) {//get fight by id
+    const fight = fightRepository.getOne({ id });
+    return fight || null;
+  }
+  create(fightData) {//create new fight
+    const newFight = fightRepository.create(fightData);//create new fight with provided data and return it
+    return newFight;
+  }
+  delete(id) {//delete fight by id
+    const fight = this.getOne(id);
+    if (!fight) {//if there is no fight with such id we return null
+      return null;
+    }
+    const deletedFight = fightRepository.delete(id);
+    return deletedFight;
+  }
 }
-
 const fightersService = new FightersService();
-
 export { fightersService };
